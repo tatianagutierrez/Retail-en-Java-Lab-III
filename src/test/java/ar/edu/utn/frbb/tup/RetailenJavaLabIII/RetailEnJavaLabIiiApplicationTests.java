34503bfb2e9ca054437305ec2c39a6e94e14db89
tatianagutierrez <tatiana7gutierrez@gmail.com>
2023-05-07 11:54:52 -0300

    Primer commit

---
+++ src/test/java/ar/edu/utn/frbb/tup/RetailenJavaLabIII/RetailEnJavaLabIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frbb.tup.RetailenJavaLabIII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailEnJavaLabIiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
